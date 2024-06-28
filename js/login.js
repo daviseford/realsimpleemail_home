a2635003422d39b8744430d4e5a94b5ed381a766
Davis Ford <daviseford@users.noreply.github.com>
2024-06-28 09:37:12 -0600

    Update login page

--- js/login.js
+++ js/login.js
@@ -2,25 +2,23 @@
  * Created by Davis on 2/22/2016.
  */
 $(document).ready(function () {
+  $("#companyNameForm").submit(function (event) {
+    // cancels the form submission
+    event.preventDefault();
+  });
 
-    $("#companyNameForm").submit(function (event) {
-        // cancels the form submission
-        event.preventDefault();
-    });
-
-    $('#goBtn').click(function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-        var entry = $.trim($('#companyNameInput').val().toLowerCase());
-        if (entry == 'wjma') {
-            window.location.replace("http://realsimpleemail.com/wjma/dashboard.php");
-        } else if (entry === 'teaparty') {
-            window.location.replace("http://realsimpleemail.com/teaparty/dashboard.php");
-        } else {
-            $('#warningSpan').removeClass('hidden');
-        }
-
-    })
-
-
+  $("#goBtn").click(function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+    var entry = $.trim($("#companyNameInput").val().toLowerCase());
+    if (entry == "wjma") {
+      window.location.replace("http://realsimpleemail.com/wjma/dashboard.php");
+    } else if (entry === "teaparty") {
+      window.location.replace(
+        "http://realsimpleemail.com/teaparty/dashboard.php"
+      );
+    } else {
+      $("#warningSpan").removeClass("invisible");
+    }
+  });
 });
--- login.php
+++ login.php
@@ -1,74 +1,53 @@
+<!DOCTYPE html>
 <html>
-<head><title>Company Login - RealSimpleEmail</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-    <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"
-          type="text/css">
-    <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css" rel="stylesheet"
-          type="text/css">
-    <script src="js/login.js" async></script>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- PAGE settings -->
+    <title>Company Login - RealSimpleEmail</title>
+    <!-- CSS dependencies -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css" />
+    <link rel="stylesheet" href="wireframe.css" />
 </head>
-<body>
 
-<div class="navbar navbar-inverse navbar-static-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse"><span
-                    class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span
-                    class="icon-bar"></span></button>
-            <a class="navbar-brand" href="index.html"><span>RealSimpleEmail</span></a></div>
-        <div class="collapse navbar-collapse" id="navbar-ex-collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <a href="index.html">Home</a>
-                </li>
-                <li class="active">
-                    <a href="#">Login<br></a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div>
-<div class="section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="section">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-12"><h1 class="text-center">Please enter your company's account name</h1>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-offset-3 col-md-6">
-                                <form role="form" id="companyNameForm">
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <input type="text" class="form-control"
-                                                   placeholder="" id="companyNameInput">
-                                            <span class="input-group-btn">
-                                            <button class="btn btn-success" id="goBtn" role="button" type="submit">
-                                                Go
-                                            </button>
-                                            </span>
-                                        </div>
-                                        <span class="text-danger hidden" id="warningSpan">
-                                            <small>Try again. This company
-                                                does not exist in our database.
-                                            </small>
-                                        </span>
-
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+<body class="bg-light">
+    <div class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="text-center col-md-7 mx-auto">
+                    <i class="fa d-block fa-bullseye fa-5x mb-4 text-info"></i>
+                    <h2><b>Log in to RSE</b></h2>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    <div class="">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 order-md-2">
+                    <form class="card p-2 my-4">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="" id="companyNameInput" />
+                            <div class="input-group-append">
+                                <button class="btn btn-secondary" id="goBtn" role="button" type="submit">
+                                    Go
+                                </button>
+                            </div>
+                        </div>
+                        <small class="text-muted">Please enter your company's account name</small>
+                        <div id="warningSpan" class="text-danger invisible">
+                            Try again. This company does not exist in our database.
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="js/login.js" async></script>
 </body>
+
 </html>
